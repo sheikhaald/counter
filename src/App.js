7cdc6ac7ded85349a3d34b8e9e3af027d085159a
sheikhaald <smaldeai98@gmail.com>
2023-10-03 18:49:19 +0300

    push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const addOne = () => {
+    return setCounter(counter + 1);
+  };
+  const minOne = () => {
+    return setCounter(counter - 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={addOne}>+</button>
+      <h1>{counter}</h1>
+      <button onClick={minOne}>-</button>
     </div>
   );
 }
